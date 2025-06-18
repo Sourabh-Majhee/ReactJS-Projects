5375e558a026979a86cd6429a7e705c5b09452e1
Sourabh Majhee <sourabhmajhee605@gmail.com>
2025-06-18 09:00:36 +0530

    Removed default codes from vite@react, for App.jsx, etc and created new files, todo.jsx , todoItems.jsx along with there css files

---
+++ Learning/ToDo/src/components/todo.jsx
@@ -0,0 +1,10 @@
+import './css/todo.css'
+const todo = () => {
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default todo
---
+++ Learning/ToDo/src/components/todoItems.jsx
@@ -0,0 +1,11 @@
+import './css/todoItems.css'
+
+const todoItems = () => {
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default todoItems
